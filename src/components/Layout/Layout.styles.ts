1f0388fd5da2c06ea6f959b6a2a33b6678b77938
Jose Alejandro <jose-a-mendez@hotmail.com>
2022-12-12 21:57:42 -0300

    Align footer text

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import styled from "styled-components";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { theme } from "../utils/theme";
 
 export const Title = styled(Typography)`
@@ -15,11 +15,15 @@ export const Footer = styled.footer`
 export const FooterText = styled.span`
   color: ${theme.colors.grey[1]};
   font-size: 1.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const IconCustom = styled(FavoriteIcon)`
   &.MuiSvgIcon-root {
     color: ${theme.colors.red[0]};
     font-size: 1.1rem;
+    margin: 0 0.2rem;
   }
 `;
